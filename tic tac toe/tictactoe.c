ece5a0a17e580d190956db4fbd512deef7d25940
eric1988m <56766342+eric1988m@users.noreply.github.com>
2021-11-16 20:31:10 -0600

    printing the board

--- tic tac toe/tictactoe.c
+++ tic tac toe/tictactoe.c
@@ -3,17 +3,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// print matrix
+void printMatrix(char val);
+unsigned char mat[3][3] = { '_', '_', '_', '_', '_', '_', '_', '_', '_' };
+unsigned char onlyOnce = 0;
+
+
 int main()
 {
-	unsigned char user1[30], user2[30];
-
+	unsigned char user1[35], user2[35], winner;
+	int ival, jval;
 
 	printf("\n Enter name of user1 :");
 	scanf_s(user1);
+	fflush(stdin);
 	
 	
-	printf("\n Enter name of user2 :");
+	printf("Enter name of user2 :");
 	scanf_s(user2);
+	fflush(stdin);
 
 
+	system("clear");
+	printf("\n\n\n\n");
+	if (!onlyOnce)
+	{
+		printMatrix(0);
+		onlyOnce = 1;
+	}
+	else
+	{
+		printMatrix(1);
+	}
+	
+	printf("\n");
+	return 0;
+}
+
+void printMatrix(char val)
+{
+
+	unsigned char i, j;
+	if (!val)
+	{
+
+		for (i = 0; i < 3; i++)
+		{
+			printf("\t\t\t");
+			for (j = 0; j < 3; j++)
+			{
+				printf("[%3c ] ", mat[i][j]);
+			}
+
+		}
+
+		return;
+	}
+	for (i = 0; i < 3; i++)
+	{
+		printf("\t\t\t");
+		for (j = 0; j < 3; j++)
+		{
+			printf("[%3c ] ", mat[i][j]);
+		}
+		printf("\n");
+	}
+	return;
 }
