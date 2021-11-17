01d0beee1698606523035ae0edb3c9288b648392
eric1988m <56766342+eric1988m@users.noreply.github.com>
2021-11-16 19:40:43 -0600

    getting the users name

--- tic tac toe/tictactoe.c
+++ tic tac toe/tictactoe.c
@@ -5,9 +5,15 @@
 
 int main()
 {
+	unsigned char user1[30], user2[30];
+
 
 	printf("\n Enter name of user1 :");
-	printf("\n Test to make sure its updating")
+	scanf_s(user1);
+	
+	
 	printf("\n Enter name of user2 :");
+	scanf_s(user2);
+
 
 }
