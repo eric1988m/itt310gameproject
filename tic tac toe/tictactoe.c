62fdc66492dbbaece26679350d3009407f22289f
eric1988m <56766342+eric1988m@users.noreply.github.com>
2021-11-20 18:59:04 -0600

    commenting on the code

--- tic tac toe/tictactoe.c
+++ tic tac toe/tictactoe.c
@@ -5,49 +5,65 @@
 
 // print matrix
 void printMatrix(char val);
+// update value in matrix
 void inserValue(int i, int j, unsigned char user);
+
+
 unsigned char mat[3][3] = { '_', '_', '_', '_', '_', '_', '_', '_', '_' };
 unsigned char onlyOnce = 0;
 
 
 int main()
 {
+	// unsigned char for users 1 and users
 	unsigned char user1[35], user2[35];
 
+
+	// print statement in quotation 
 	printf("\nEnter name of user1 : ");
+	// get user input 
 	scanf_s(user1);
-	fflush(stdin);
+
 	
+	// print statement in quotation
 	printf("Enter name of user2 :");
+	// get user input
 	scanf_s(user2);
-	fflush(stdin);
+	
 	
 	
 
 
-	system("clear");
+	// print 4 new lines
 	printf("\n\n\n\n");
+	// start of if statement
 	if (!onlyOnce)
 	{
+		// print matrix and line up
 		printMatrix(1);
+		// onlyonce equals 1
 		onlyOnce = 1;
 	}
+	// else 
 	else
 	{
+		//printMatrix 
 		printMatrix(1);
 	}
-	
+	// print a new line
 	printf("\n");
+	// end function
 	return 0;
 	
 }
-
+// print matrix parameters
 void printMatrix(char val)
 {
-
+	// unsigned char i and j 
 	unsigned char i, j;
 	if (!val)
 	{
+		/* for blank matridx*/
 
 		for (i = 0; i < 3; i++)
 		{
@@ -56,6 +72,8 @@ void printMatrix(char val)
 			{
 				printf("[%3c ] ", mat[i][j]);
 			}
+
+			printf("\n");
 
 		}
 
